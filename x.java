1a1496c6527c8fc5b6c73e99a19f963dd52ee53c
Ashtalakshmi8 <ASHTALAKSHMI8>
2021-12-20 22:59:32 +0530

    thank you

---
+++ x.java
@@ -0,0 +1,20 @@
+class laptop{
+public static void main(String [] a){
+System.out.println(ram);
+System.out.println(ram);
+System.out.println(ram);
+System.out.println(ram);
+System.out.println(ram);
+System.out.println(ram);
+System.out.println(ram);
+System.out.println(ram);
+}
+static int price;
+static byte price;
+static char price;
+static long price;
+static double price;
+static short price;
+static boolean price;
+static float price;
+}
